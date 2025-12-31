519c858d31d71acb091209ca312f2b54b1ba656c
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-12-31 19:47:29 +0700

    feat: added new pistol Glock-19

--- 42/media/lua/server/items/tgp_Distributions.lua
+++ 42/media/lua/server/items/tgp_Distributions.lua
@@ -5,6 +5,20 @@ local tgpDistribution = {
     items = {
       "BattleRifle", 2,
       "ScarH_Mag", 8,
+      "Glock19", 20,
+			"Glock19", 10,
+    }
+  },
+
+  FirearmWeapons = {
+    items = {
+      "Glock19", 8,
+    }
+  },
+
+  FirearmWeapons_Mid = {
+    items = {
+      "Glock19", 8,
     }
   },
   
@@ -12,6 +26,7 @@ local tgpDistribution = {
     items = {
       "BattleRifle", 1,
       "ScarH_Mag", 4,
+      "Glock19", 8,
     }
   },
   
@@ -21,11 +36,145 @@ local tgpDistribution = {
       "ScarH_Mag", 0.1,
     }
   },
+
+  LockerArmyBedroomHome = {
+    items = {
+      "Glock19", 0.5,
+    }
+  },
   
   PawnShopGunsSpecial = {
     items = {
       "BattleRifle", 0.25,
       "ScarH_Mag", 10,
+      "Glock19", 8,
+    }
+  },
+
+  PlankStashGun = {
+    items = {
+      "Glock19", 10,
+    }
+  },
+
+  PoliceDesk = {
+    items = {
+      "Glock19", 0.1,
+    }
+  },
+
+  PoliceEvidence = {
+    items = {
+      "Glock19", 8,
+    }
+  },
+
+  PoliceOutfit = {
+    items = {
+      "Glock19", 10,
+    }
+  },
+
+  PoliceStorageGuns = {
+    items = {
+      "Glock19", 50,
+      "Glock19", 20,
+    }
+  },
+
+  PoliceTools = {
+    items = {
+      "Glock19", 20,
+    }
+  },
+
+  SecurityDesk = {
+    items = {
+      "Glock19", 0.1,
+    }
+  },
+
+  SecurityLockers = {
+    items = {
+      "Glock19", 0.1,
+    }
+  },
+
+  GunStoreGuns = {
+    items = {
+      "Glock19", 10,
+    }
+  },
+
+  GunStorePistols = {
+    items = {
+      "Glock19", 50,
+    }
+  },
+
+  DrugLabGuns = {
+    items = {
+      "Glock19", 8,
+    }
+  },
+
+  BedroomDresser = {
+    items = {
+      "Glock19", 0.5,
+    }
+  },
+
+  BedroomDresserClassy = {
+    items = {
+      "Glock19", 0.5,
+    }
+  },
+
+  BedroomDresserRedneck = {
+    items = {
+      "Glock19", 0.5,
+    }
+  },
+
+  BedroomSidetable = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+
+  BedroomSidetableClassy = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+
+  BedroomSidetableRedneck = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+
+  OfficeDeskHome = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+
+  OfficeDeskHomeClassy = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+  
+  DresserGeneric = {
+    items = {
+      "Glock19", 0.05,
+    }
+  },
+
+  GarageFirearms = {
+    items = {
+      "Glock19", 4,
     }
   }
 }
